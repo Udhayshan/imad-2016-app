d1c9dc8c362ee1e392b3482f6ca0d51476c59d83
Udhayshan <udhay.shan@gmail.com>
2016-09-21 08:02:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 
 var element = document.getElementById('maintext');
-element.innerHTML = 'JavaScript Rocks'
+element.innerHTML = 'JavaScript Rocks';
+
+var img = document.getElementById('monster');
+
+img.onclick = function()
+{
+    img.style.marginleft = '80px';
+}
